fbda0f470e4388a75f54d9f79939bb1fe4829b52
Dtesch9 <alucard.s8123@gmail.com>
2020-07-26 11:56:02 -0300

    feat: hook Loading

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,14 +1,23 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
-import { SafeAreaView, ScrollView, View, Text, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { StatusBar, SafeAreaView } from 'react-native';
+
+import AppProvider from './hooks';
+import Routes from './routes';
 
 const App: React.FC = () => {
   return (
-    <>
-      <StatusBar barStyle="light-content" />
-      <SafeAreaView>
-        <ScrollView contentInsetAdjustmentBehavior="automatic" />
-      </SafeAreaView>
-    </>
+    <NavigationContainer>
+      <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+
+      <AppProvider>
+        <SafeAreaView style={{ flex: 1, backgroundColor: '#312e38' }}>
+          <Routes />
+        </SafeAreaView>
+      </AppProvider>
+    </NavigationContainer>
   );
 };
 
