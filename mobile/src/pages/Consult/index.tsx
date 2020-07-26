d3e95deea030dd0ce3c7b0ef0f7f9f366a619e24
Dtesch9 <alucard.s8123@gmail.com>
2020-07-26 13:30:07 -0300

    feat: Consult Layout

--- mobile/src/pages/Consult/index.tsx
+++ mobile/src/pages/Consult/index.tsx
@@ -1,10 +1,86 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useMemo } from 'react';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Wrapper,
+  Logo,
+  Form,
+  OriginContainer,
+  OriginText,
+  DestinationContainer,
+  DestinationText,
+  PlanContainer,
+  PlanText,
+  ButtonsList,
+  ButtonBox,
+  CustomRectButton,
+  ButtonText,
+} from './styles';
 
 const Consult: React.FC = () => {
-  return <Text>olá consult</Text>;
+  const origins = useMemo(() => {
+    return ['011', '016', '017', '018'];
+  }, []);
+
+  const destinations = useMemo(() => {
+    return ['011', '016', '017', '018'];
+  }, []);
+
+  const plans = useMemo(() => {
+    return ['30', '60', '120'];
+  }, []);
+
+  return (
+    <Container>
+      <Wrapper>
+        <Logo />
+
+        <Form>
+          <OriginContainer>
+            <OriginText>Informe o DDD de origem</OriginText>
+
+            <ButtonsList>
+              {origins.map((origin, index) => (
+                <ButtonBox key={origin} isFirst={index}>
+                  <CustomRectButton>
+                    <ButtonText>{origin}</ButtonText>
+                  </CustomRectButton>
+                </ButtonBox>
+              ))}
+            </ButtonsList>
+          </OriginContainer>
+
+          <DestinationContainer>
+            <DestinationText>Informe o DDD de destino</DestinationText>
+
+            <ButtonsList>
+              {destinations.map((destination, index) => (
+                <ButtonBox key={`${destination}${index}`} isFirst={index}>
+                  <CustomRectButton>
+                    <ButtonText>{destination}</ButtonText>
+                  </CustomRectButton>
+                </ButtonBox>
+              ))}
+            </ButtonsList>
+          </DestinationContainer>
+
+          <PlanContainer>
+            <PlanText>Informe su Plano FaleMais</PlanText>
+
+            <ButtonsList>
+              {plans.map((plan, index) => (
+                <ButtonBox key={plan} isFirst={index}>
+                  <CustomRectButton>
+                    <ButtonText>{plan}</ButtonText>
+                  </CustomRectButton>
+                </ButtonBox>
+              ))}
+            </ButtonsList>
+          </PlanContainer>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default Consult;
--- mobile/src/pages/Consult/styles.tsx
+++ mobile/src/pages/Consult/styles.tsx
@@ -1,3 +1,102 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
-export const Container = styled.View``;
+import logo from '../../assets/logo.png';
+
+interface ButtonBoxProps {
+  isFirst: number;
+}
+
+export const Container = styled.View`
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Wrapper = styled.View`
+  width: 95%;
+  align-items: center;
+  margin-bottom: 12px;
+`;
+
+export const Logo = styled.Image.attrs({
+  source: logo,
+})`
+  width: 200px;
+  height: 200px;
+`;
+
+export const Form = styled.View`
+  width: 100%;
+  align-items: center;
+`;
+
+const originDestinationPlanTextCss = css`
+  margin-bottom: 18px;
+  margin-top: 18px;
+  color: rgba(255, 255, 255, 0.4);
+  font-size: 20px;
+  font-family: 'RobotoSlab-Light';
+`;
+
+const originDestinationPlanContainerCss = css`
+  max-width: 100%;
+`;
+
+export const OriginContainer = styled.View`
+  ${originDestinationPlanContainerCss};
+`;
+
+export const OriginText = styled.Text`
+  ${originDestinationPlanTextCss};
+`;
+
+export const DestinationContainer = styled.View`
+  ${originDestinationPlanContainerCss};
+`;
+
+export const DestinationText = styled.Text`
+  ${originDestinationPlanTextCss};
+  text-align: right;
+`;
+
+export const PlanContainer = styled.View`
+  ${originDestinationPlanContainerCss};
+`;
+
+export const PlanText = styled.Text`
+  ${originDestinationPlanTextCss};
+  text-align: center;
+`;
+
+export const ButtonsList = styled.View`
+  flex-direction: row;
+  justify-content: center;
+`;
+
+export const ButtonBox = styled.View<ButtonBoxProps>`
+  width: 85px;
+  height: 85px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  border-width: 2px;
+  border-style: solid;
+  border-color: #ff4400;
+  border-radius: 4px;
+
+  margin-left: ${({ isFirst }) => isFirst && '8px'};
+`;
+
+export const CustomRectButton = styled(RectButton).attrs({
+  rippleColor: '#ff7700',
+})`
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  border-radius: 4px;
+`;
+
+export const ButtonText = styled.Text`
+  color: #ff4400;
+`;
